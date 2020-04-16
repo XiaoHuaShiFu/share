44ddc554e7b55520c73cfd8a30ecdafa5d760d54
imooc <827032783@qq.com>
2020-04-17 00:31:43 +0800

    1

---
+++ src/test/java/top/xiaohuashifu/share/ShareApplicationTests.java
@@ -0,0 +1,14 @@
+package top.xiaohuashifu.share;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
